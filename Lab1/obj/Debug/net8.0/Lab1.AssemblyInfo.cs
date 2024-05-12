cc0b26e04cdb4755f83c62d6bb68f136a0f0a199
victor.amisi <victor.amisi@dreampayments.com>
2024-05-12 17:32:11 -0400

    Completes the assignment

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df135b6c80a1846ad530e3befe4db09699f80e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe375bc960dc654851307c2314080416de95530")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
